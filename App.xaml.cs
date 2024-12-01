484ead2a20dd54d6541733eb7d3236222aeeeac1
DevTKSS <info@technische-konstruktion.com>
2024-12-01 22:37:21 +0100

    moved NavigationRootPage into MainWindow hoping Window is right place for this

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,6 +2,10 @@
 using WorkdayCalculator.Views;
 using WorkdayCalculator.NavigationViews;
 using WorkdayCalculator.Helpers;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using Windows.ApplicationModel.Activation;
+using Microsoft.UI.Xaml.Navigation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,40 +25,63 @@ public partial class App : Application
         this.InitializeComponent();
     }
 
-    private static Window? startupWindow;
+    private static Window? MainWindow;
 
     /// <summary>
     /// Invoked when the application is launched.
     /// </summary>
     /// <param name="args">Details about the launch request and process.</param>
-    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs e)
     {
+        MainWindow = new Window();
 
-        startupWindow = new MainWindow();
-        startupWindow.ExtendsContentIntoTitleBar = true;
-        startupWindow.Activate();
+        if (MainWindow.Content is not Frame rootFrame)
+        {
+            // Create a Frame to act as the navigation context and navigate to the first page.
+            rootFrame = new Frame();
+            rootFrame.NavigationFailed += OnNavigationFailed;
 
-    }
-
-    /// Code copy-pasted from the WinUI 3 Windowing sample
+            // Place the frame in the current Window.
+            MainWindow.Content = rootFrame;
+        }
     
-    /// <summary>
-    /// Creates a new Window instance for the application.
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    private void createNewWindow_Click(object sender, RoutedEventArgs e)
-    {
-        // C# code to create a new window
-        var newWindow = WindowHelper.CreateWindow();
-        var rootPage = new NavigationRootPage();
-        rootPage.RequestedTheme = ThemeHelper.RootTheme;
-        newWindow.Content = rootPage;
-        newWindow.Activate();
+        if(rootFrame.Content == null)
+        {
+            // When the navigation stack isn't restored navigate to the first page,
+            // configuring the new page by passing required information as a navigation
+            // parameter
+            rootFrame.Navigate(typeof(NavigationRootPage), e.Arguments);
+        }
 
-        // C# code to navigate in the new window
-        var targetPageType = typeof(HomePage);
-        string targetPageArguments = string.Empty;
-        rootPage.Navigate(targetPageType, targetPageArguments);
+        MainWindow.ExtendsContentIntoTitleBar = true;
+        MainWindow.Activate();
+
     }
+
+    private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+    {
+        throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+    }
+    ///// Code copy-pasted from the WinUI 3 Windowing sample
+
+    ///// <summary>
+    ///// Creates a new Window instance for the application.
+    ///// </summary>
+    ///// <param name="sender"></param>
+    ///// <param name="e"></param>
+    //private void createNewWindow_Click(object sender, RoutedEventArgs e)
+    //{
+    //    // C# code to create a new window
+    //    var newWindow = WindowHelper.CreateWindow();
+    //    var rootPage = new NavigationRootPage();
+    //    rootPage.RequestedTheme = ThemeHelper.RootTheme;
+    //    newWindow.Content = rootPage;
+    //    newWindow.Activate();
+
+    //    // C# code to navigate in the new window
+    //    var targetPageType = typeof(HomePage);
+    //    string targetPageArguments = string.Empty;
+    //    rootPage.Navigate(targetPageType, targetPageArguments);
+    //}
+
 }
--- Helpers/WindowHelper.cs
+++ Helpers/WindowHelper.cs
@@ -8,97 +8,113 @@
 //
 //*********************************************************
 
-using Microsoft.UI;
-using Microsoft.UI.Windowing;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Media;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
 using Windows.Storage;
-using WinRT.Interop;
 
-///Copy from WinUI 3 Galley App Repo with adjusted Project Namespace
-namespace WorkdayCalculator.Helpers
+/// Copy from WinUI 3 Galley App Repo with adjusted Project Namespace
+/// removed Win32 operations and added a helper method to get the local folder
+namespace WorkdayCalculator.Helpers;
+
+// Helper class to allow the app to find the Window that contains an
+// arbitrary UIElement (GetWindowForElement).  To do this, we keep track
+// of all active Windows.  The app code must call WindowHelper.CreateWindow
+// rather than "new Window" so we can keep track of all the relevant
+// windows.  In the future, we would like to support this in platform APIs.
+public class WindowHelper
 {
-    // Helper class to allow the app to find the Window that contains an
-    // arbitrary UIElement (GetWindowForElement).  To do this, we keep track
-    // of all active Windows.  The app code must call WindowHelper.CreateWindow
-    // rather than "new Window" so we can keep track of all the relevant
-    // windows.  In the future, we would like to support this in platform APIs.
-    public class WindowHelper
+    public static Window CreateWindow()
     {
-        public static Window CreateWindow()
+        Window newWindow = new Window
         {
-            Window newWindow = new Window
+            SystemBackdrop = new MicaBackdrop()
+        };
+        TrackWindow(newWindow);
+        return newWindow;
+    }
+
+    public static void TrackWindow(Window window)
+    {
+        window.Closed += (sender,args) => {
+            _activeWindows.Remove(window);
+        };
+        _activeWindows.Add(window);
+    }
+
+    public static AppWindow GetAppWindow(Window window)
+    {
+        return window.AppWindow;
+    }
+
+    /// <summary>
+    /// Get the Window that contains the given UIElement.
+    /// Will return null if no window is found.
+    /// </summary>
+    /// <param name="element"></param>
+    /// <returns></returns>
+    public static Window? GetWindowForElement(UIElement element)
+    {
+        if (element.XamlRoot != null)
+        {
+            foreach (Window window in _activeWindows)
             {
-                SystemBackdrop = new MicaBackdrop()
-            };
-            TrackWindow(newWindow);
-            return newWindow;
-        }
-
-        public static void TrackWindow(Window window)
-        {
-            window.Closed += (sender,args) => {
-                _activeWindows.Remove(window);
-            };
-            _activeWindows.Add(window);
-        }
-
-        public static AppWindow GetAppWindow(Window window)
-        {
-            IntPtr hWnd = WindowNative.GetWindowHandle(window);
-            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
-            return AppWindow.GetFromWindowId(wndId);
-        }
-
-        public static Window GetWindowForElement(UIElement element)
-        {
-            if (element.XamlRoot != null)
-            {
-                foreach (Window window in _activeWindows)
+                if (element.XamlRoot == window.Content.XamlRoot)
                 {
-                    if (element.XamlRoot == window.Content.XamlRoot)
-                    {
-                        return window;
-                    }
+                    return window;
                 }
             }
-            return null;
         }
-        // get dpi for an element
-        public static double GetRasterizationScaleForElement(UIElement element)
+        return null;
+    }
+
+    // get dpi for an element
+    public static double GetRasterizationScaleForElement(UIElement element)
+    {
+        if (element.XamlRoot != null)
         {
-            if (element.XamlRoot != null)
+            foreach (Window window in _activeWindows)
             {
-                foreach (Window window in _activeWindows)
+                if (element.XamlRoot == window.Content.XamlRoot)
                 {
-                    if (element.XamlRoot == window.Content.XamlRoot)
-                    {
-                        return element.XamlRoot.RasterizationScale;
-                    }
+                    return element.XamlRoot.RasterizationScale;
                 }
             }
-            return 0.0;
         }
+        return 0.0;
+    }
 
-        public static List<Window> ActiveWindows { get { return _activeWindows; }}
-
-        private static List<Window> _activeWindows = new List<Window>();
-
-        public static StorageFolder GetAppLocalFolder()
+    public static List<Window> ActiveWindows 
+    {
+        get
         {
-            StorageFolder localFolder;
-            if (!NativeHelper.IsAppPackaged)
-            {
-                localFolder = Task.Run(async () => await StorageFolder.GetFolderFromPathAsync(System.AppContext.BaseDirectory)).Result;
-            }
-            else
-            {
-                localFolder = ApplicationData.Current.LocalFolder;
-            }
-            return localFolder;
+            return _activeWindows; 
         }
+    }
+
+    private static readonly List<Window> _activeWindows = new List<Window>();
+
+    public static bool isPackaged()
+    {
+        return System.AppContext.BaseDirectory.Contains("WindowsApps");
+    }
+
+    public static StorageFolder GetAppLocalFolder()
+    {
+
+        StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+        if (isPackaged())
+        {
+            localFolder = Task.Run(async () => await StorageFolder.GetFolderFromPathAsync(System.AppContext.BaseDirectory)).Result;
+        }
+        else
+        {
+            localFolder = ApplicationData.Current.LocalFolder;
+        }
+        return localFolder;
     }
 }
